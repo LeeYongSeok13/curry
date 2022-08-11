ae8cf24fedd4ca155284fea3f5e5a18691327720
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-08-11 17:49:12 +0900

    20220811

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ $(function () {
         arrows: false,
         dots: true,
     });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(300);
+        } else {
+            $('.toTop').fadeOut(1000);
+        }
+    });
+
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 500);
+    })
 });
